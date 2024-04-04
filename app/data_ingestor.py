059b0a2c65fe241b44e50d4ef745b7f0a9e88350
Ryuuandrei <andreirusei@gmail.com>
2024-04-04 17:06:29 +0300

    add comments

--- app/data_ingestor.py
+++ app/data_ingestor.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 class DataIngestor:
     def __init__(self, csv_path: str):
-        # TODO: Read csv from csv_path
         self.df = pd.read_csv(csv_path)
 
         self.questions_best_is_min = [
@@ -23,35 +22,114 @@ class DataIngestor:
         ]
 
     def states_mean(self, question: str):
+        """
+        Calculate the mean of the 'Data_Value' column for a given question, grouped by 'LocationDesc'.
+
+        Parameters:
+            question (str): The question for which to calculate the mean.
+
+        Returns:
+            str: The mean values for each location in JSON format.
+        """
         return lambda : self.df.loc[self.df['Question'] == question] \
             .groupby('LocationDesc')['Data_Value'].mean().sort_values(ascending=True).to_json()
         
     def state_mean(self, question: str, state: str):
+        """
+        Calculate the mean value of a specific question for a given state.
+
+        Parameters:
+        - question (str): The question for which the mean value is calculated.
+        - state (str): The state for which the mean value is calculated.
+
+        Returns:
+        - str: A JSON string containing the mean value of the question for the state.
+        """
         return lambda : json.dumps({
             state : self.df.loc[(self.df['Question'] == question) & (self.df['LocationDesc'] == state)]['Data_Value'].mean()
-            })
+        })
     
     def best5(self, question: str):
+        """
+        Returns the top 5 locations with the highest or lowest average data value for a given question.
+
+        Parameters:
+            question (str): The question for which the average data value is calculated.
+
+        Returns:
+            str: A JSON string containing the top 5 locations and their average data values.
+        """
         return lambda : self.df.loc[self.df['Question'] == question] \
             .groupby('LocationDesc')['Data_Value'].mean().sort_values(ascending=(question in self.questions_best_is_min)).head(5).to_json()
     
     def worst5(self, question: str):
+        """
+        Returns the worst 5 locations based on the average data value for a given question.
+
+        Args:
+            question (str): The question for which to calculate the worst 5 locations.
+
+        Returns:
+            str: A JSON string containing the worst 5 locations and their average data values.
+        """
         return lambda : self.df.loc[self.df['Question'] == question] \
             .groupby('LocationDesc')['Data_Value'].mean().sort_values(ascending=(question in self.questions_best_is_max)).head(5).to_json()
     
     def global_mean(self, question: str):
-        return lambda : json.dumps({
+        """
+        Calculate the global mean of the specified question.
+
+        Parameters:
+        - question (str): The question for which to calculate the global mean.
+
+        Returns:
+        - str: A JSON string containing the global mean value.
+
+        Example Usage:
+        ```
+        data_ingestor = DataIngestor()
+        result = data_ingestor.global_mean("Question 1")
+        print(result)
+        ```
+        """
+        return lambda: json.dumps({
             "global_mean": self.df.loc[self.df['Question'] == question]['Data_Value'].mean()
-            })
+        })
     
     def diff_from_mean(self, question: str):
+        """
+        Calculates the difference from the mean for a given question.
+
+        Parameters:
+        - question (str): The question for which to calculate the difference.
+
+        Returns:
+        - function: A function that calculates the difference from the mean for the given question.
+        """
+
         def aux():
             gm = self.df.loc[self.df['Question'] == question]['Data_Value'].mean()
             return self.df.loc[self.df['Question'] == question] \
                 .groupby('LocationDesc')['Data_Value'].apply(lambda x: gm - x.mean()).to_json()
+
         return aux
     
     def state_diff_from_mean(self, question: str, state: str):
+        """
+        Calculates the difference between the mean value of a question and the mean value of that question in a specific state.
+
+        Args:
+            question (str): The question for which the difference is calculated.
+            state (str): The state for which the difference is calculated.
+
+        Returns:
+            function: A function that calculates the difference between the mean values.
+
+        Example:
+            diff_func = state_diff_from_mean("Question 1", "California")
+            diff = diff_func()
+            print(diff)  # Output: {"California": -0.5}
+        """
         def aux():
             gm = self.df.loc[self.df['Question'] == question]['Data_Value'].mean()
             return json.dumps({
@@ -60,12 +138,41 @@ class DataIngestor:
         return aux
     
     def mean_by_category(self, question: str):
+        """
+        Calculate the mean of the 'Data_Value' column by category.
+
+        Parameters:
+        - question (str): The question for which the mean is calculated.
+
+        Returns:
+        - function: A function that calculates the mean by category when called.
+
+        Example usage:
+        ```
+        mean_func = mean_by_category('Question1')
+        result = mean_func()
+        print(result)
+        ```
+
+        Note: The returned function should be called to get the mean by category.
+        """
         def aux():
             return self.df.loc[self.df['Question'] == question] \
                 .groupby(['LocationDesc', 'StratificationCategory1', 'Stratification1'])['Data_Value'].mean().to_json()
         return aux
     
     def state_mean_by_category(self, question: str, state: str):
+        """
+        Returns a JSON string containing the mean values of 'Data_Value' grouped by 'StratificationCategory1' and 'Stratification1'
+        for a given 'question' and 'state'.
+
+        Parameters:
+            question (str): The question for which the mean values are calculated.
+            state (str): The state for which the mean values are calculated.
+
+        Returns:
+            str: A JSON string containing the mean values grouped by 'StratificationCategory1' and 'Stratification1'.
+        """
         def aux():
             return json.dumps({
                 state : json.loads(self.df.loc[(self.df['Question'] == question) & (self.df['LocationDesc'] == state)] \
--- app/task_runner.py
+++ app/task_runner.py
@@ -17,10 +17,17 @@ class ThreadPool:
             thread.start()
 
     def shutdown(self):
-        self.queue.join()
-        for thread in self.threads:
-            thread.shutdown = True
-            thread.join()
+            """
+            Shuts down the task runner by joining all the threads and stopping their execution.
+
+            This method waits for all the tasks in the queue to be processed and then stops all the threads
+            by setting their `shutdown` flag to True and joining them.
+
+            """
+            self.queue.join()
+            for thread in self.threads:
+                thread.shutdown = True
+                thread.join()
 
 
 class TaskRunner(Thread):
@@ -31,13 +38,25 @@ class TaskRunner(Thread):
         self.shutdown = False
     
     def run(self):
-        while True:
-            try:
-                job = self.queue.get(block=True, timeout=1)
-                with open(f'results/{job[0]}', 'a') as f:
-                    f.write(job[1]())
-                self.queue.task_done()
-                self.tasks.append(job[0])
-            except Exception as e:
-                if self.shutdown:
-                    break
+            """
+            Executes the tasks in the queue until the shutdown flag is set.
+
+            This method continuously retrieves tasks from the queue, executes them, and writes the results to the corresponding files.
+            If an exception occurs during task execution, it is caught and the loop continues.
+            The method also appends the completed task to the list of tasks.
+
+            Note: This method will block if the queue is empty, waiting for new tasks to be added.
+
+            Returns:
+                None
+            """
+            while True:
+                try:
+                    job = self.queue.get(block=True, timeout=1)
+                    with open(f'results/{job[0]}', 'a') as f:
+                        f.write(job[1]())
+                    self.queue.task_done()
+                    self.tasks.append(job[0])
+                except Exception as e:
+                    if self.shutdown:
+                        break
